6f5875451b5444ec39a64663b6cd02b5c3e089b2
David Ochoa <davochoa@paypal.com>
2020-06-15 01:06:14 -0600

    [master] Formulario contacto

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$phone = $_POST['phone'];
+$formcontent="From: $name \n Phone: $phone \n Message: $message";
+$recipient = "info@searco.com.gt";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "<script>location.replace('thankyou.html')</script>";
+?>
